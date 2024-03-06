9f3d19ff962d8d947571b323914a4b911fcc22aa
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-03-06 20:19:37 +0530

    theme change

--- src/theme.js
+++ src/theme.js
@@ -119,3 +119,101 @@ export const tokens = (mode) => ({
         },
       }),
 });
+
+//mui theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            nutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            nutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Philosopher", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Philosopher", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+// Define a custom hook named useMode
+export const useMode = () => {
+  // Initialize state variable 'mode' with the initial value "dark"
+  const [mode, setMode] = useState("dark");
+
+  // Use the useMemo hook to memoize the result of the function
+  const colorMode = useMemo(
+    () => ({
+      // Define a function toggleColorMode within the memoized object
+      toggleColorMode: () =>
+        // Set the 'mode' state based on the previous value
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  ); // The empty dependency array ensures that this memoized value is only recalculated if dependencies change
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+};
