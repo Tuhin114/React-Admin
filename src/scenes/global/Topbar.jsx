85b831c45edab74de414fb84cd49c7802ecbc7b8
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-03-08 18:25:11 +0530

    Topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import { InputBase } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsIcon from "@mui/icons-material/Settings";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display={"flex"} justifyContent={"space-between"}>
+      {/*Search Bar*/}
+      <Box
+        display={"flex"}
+        backgroundColor={colors.primary[400]}
+        borderRadius={"3px"}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/*Right Side Icons*/}
+      <Box display={"flex"}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "light" ? (
+            <DarkModeIcon />
+          ) : (
+            <LightModeIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlineIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Topbar;
