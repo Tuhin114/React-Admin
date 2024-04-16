7f4cf28293717f65edacb241f65bfab24ac86708
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-04-16 19:35:37 +0530

    Bar section

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -7,6 +7,7 @@ const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  //Visit nevo barchart section
   return (
     <ResponsiveBar
       data={data}
